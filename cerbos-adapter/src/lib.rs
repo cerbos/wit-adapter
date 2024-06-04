611dc70d1ffa225447ee30bd772b1f090411dc38
Dennis Buduev <dbuduev@users.noreply.github.com>
2024-06-04 17:19:11 +1200

    pass current time to check_wasi
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- cerbos-adapter/src/lib.rs
+++ cerbos-adapter/src/lib.rs
@@ -3,6 +3,7 @@ wit_bindgen::generate!({
 });
 use crate::exports::cerbos::policy::authorization as authz;
 use cerbos_hub::epdp::authorization;
+use wasi::clocks::wall_clock;
 
 mod from_wit;
 mod to_wit;
@@ -13,7 +14,7 @@ impl crate::exports::cerbos::policy::authorization::Guest for Cerbos {
     fn check(input: authz::Request) -> Result<authz::Response, _rt::String> {
         let request = from_wit::map_request(&input);
         let s = serde_json::to_string(&request).unwrap();
-        let result = authorization::check_wasi(&s);
+        let result = authorization::check_wasi(&s, wall_clock::now().seconds);
         let response: cerbos_serde::Response =
             serde_json::from_str(&result).map_err(|e| e.to_string())?;
         let response: Result<authz::Response, String> = to_wit::map_response(&response);
